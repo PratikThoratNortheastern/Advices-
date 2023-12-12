081cfaf2a0f5820d27aff78e29d58f9ac30beb41
Pratik Thorat <thorat.p@northeastern.edu>
2023-12-11 23:13:01 -0500

    Fetched data from adviceslip api and logged to console to test data out

--- advices/src/App.js
+++ advices/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    console.log(data.slip.advice);
+  }
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World!</h1>
+      <button onClick={getAdvice}>Get Advice</button>
     </div>
   );
 }
-
-export default App;
